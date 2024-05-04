215de7e40aa3ea6f81d5304c4be78c3c6a1b7de7
Abel <90abel.avr@gmail.com>
2024-05-04 04:36:51 -0300

    added expense-modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
+import { useMemo } from "react"
+import BudgetForm from "./components/BudgetForm"
+import { useBudget } from "./hooks/useBudget"
+import BudgetTracker from "./components/BudgetTracker"
+import ExpenseModal from "./components/ExpenseModal"
 
 function App() {
 
+  const {state} = useBudget()
+  const isValid = useMemo(()=> state.budget>0,[state.budget])
   return (
     <>
-     <h1>Control de gastos</h1>
+     <header className="bg-blue-600 py-7 max-h-72">
+        <h1 className="uppercase text-center font-black text-4xl text-white">Budget Manager</h1>
+     </header>
+     <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
+        {isValid ? <BudgetTracker/> : <BudgetForm/>}
+     </div>
+     {isValid && (
+        <main className="max-w-3xl mx-auto py-10">
+          <ExpenseModal/>
+        </main>
+     ) }
+    
     </>
   )
 }
