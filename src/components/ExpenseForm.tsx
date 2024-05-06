630dfe087d7634d7a061d09386cba8db179445d2
Abel <90abel.avr@gmail.com>
2024-05-06 04:01:33 -0300

    added error  message and expense list

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -4,8 +4,8 @@ import { categories } from "../data/categories";
 import DatePicker from 'react-date-picker';
 import 'react-date-picker/dist/DatePicker.css';
 import 'react-calendar/dist/Calendar.css';
-
-
+import ErrorMessage from "./ErrorMessage";
+import { useBudget } from "../hooks/useBudget";
 
 export default function ExpenseForm() {
 
@@ -15,9 +15,12 @@ export default function ExpenseForm() {
     category:"",
     date: new Date()
   })
+  const [error, setError] = useState("")
+  const{dispatch} = useBudget()
+
   const handleChange = ( e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
     const {name, value } = e.target
-    const isAmountField = ["amount".includes(name)]
+    const isAmountField = ["amount"].includes(name)
     setExpense({
       ...expense,
       [name] : isAmountField ? +value : value
@@ -30,9 +33,32 @@ export default function ExpenseForm() {
         date: value
       })    
   }
+  const handleSubmit = ( e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    // Validar
+    if(Object.values(expense).includes("")){
+      setError("Todos los campos son obligatorios")
+      return
+    }
+    //Agregar nuevo gasto   
+    dispatch({type: "add-expense", payload:{ expense }  })
+    // Reiniciar el state
+    setExpense(
+      {
+        amount:0,
+        expenseName:"",
+        category:"",
+        date: new Date()
+      }
+    )
+  }
+
   return (
-    <form className="space-y-5">
+    <form className="space-y-5" onSubmit={handleSubmit} >
       <legend className="uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2 ">Nuevo Gasto</legend>
+
+      {error && <ErrorMessage>{error}</ErrorMessage>}
+      
       <div className="flex flex-col gap-2">
         <label htmlFor="expenseName" className="text-xl">Nombre Gasto</label>
         <input type="text" 
--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -2,3 +2,13 @@ export function formatCurrency(amount: number){
 
     return Intl.NumberFormat("en-US",{style:"currency", currency:"USD"} ).format(amount) 
 }
+export function formatDate(dateStr : string) : string {
+    const dateObject = new Date(dateStr)
+    const options : Intl.DateTimeFormatOptions= {
+        weekday: "long",
+        year: "numeric",
+        month:"long",
+        day: "numeric"
+    }
+    return new Intl.DateTimeFormat("es-ES", options).format(dateObject)
+}
